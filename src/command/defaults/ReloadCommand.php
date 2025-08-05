239a4b276f22416ec57025d02819e5d2c0b0af8e
TestDev233 <tewpei1@gmail.com>
2025-08-05 13:04:45 +0200

    Update ReloadCommand.php

--- src/command/defaults/ReloadCommand.php
+++ src/command/defaults/ReloadCommand.php
@@ -25,7 +25,6 @@ namespace pocketmine\command\defaults;
 
 use pocketmine\command\CommandSender;
 use pocketmine\Server;
-use pocketmine\plugin\Plugin;
 use pocketmine\utils\TextFormat;
 use function count;
 use function strtolower;
@@ -123,6 +122,7 @@ class ReloadCommand extends VanillaCommand{
 		$sender->sendMessage(TextFormat::YELLOW . "/reload config <ops|bans|ip-bans|server>");
 		$sender->sendMessage(TextFormat::YELLOW . "/reload plugin <plugin_name>");
 		$sender->sendMessage(TextFormat::YELLOW . "/reload all");
+		$sender->sendMessage(TextFormat::RED . "Plugin reloading is no longer supported. Please restart the server.");
 		return true;
 	}
 }
