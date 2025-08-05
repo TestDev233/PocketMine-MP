e5686ac04a10b00d450a68d1b3ca8d86dfeaca17
TestDev233 <tewpei1@gmail.com>
2025-08-05 14:28:19 +0200

    Update ReloadCommand.php

--- src/command/defaults/ReloadCommand.php
+++ src/command/defaults/ReloadCommand.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+ *
+ * ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+ */
+
 declare(strict_types=1);
 
 namespace pocketmine\command\defaults;
@@ -12,57 +31,57 @@ use function strtolower;
 
 class ReloadCommand extends VanillaCommand{
 
-    public function execute(CommandSender $sender, string $label, array $args) : bool{
-        if(!$sender->hasPermission("pocketmine.command.reload")){
-            $sender->sendMessage(TextFormat::RED . "You do not have permission to use this command.");
-            return true;
-        }
+	public function execute(CommandSender $sender, string $label, array $args) : bool{
+		if(!$sender->hasPermission("pocketmine.command.reload")){
+			$sender->sendMessage(TextFormat::RED . "You do not have permission to use this command.");
+			return true;
+		}
 
-        $server = Server::getInstance();
+		$server = Server::getInstance();
 
-        if(count($args) === 0 || strtolower($args[0]) === "all"){
-            $sender->sendMessage(TextFormat::YELLOW . "Restarting all plugins...");
-            
-            $pluginManager = $server->getPluginManager();
-            $plugins = $pluginManager->getPlugins();
-            foreach($plugins as $plugin){
-                if($plugin->isEnabled()){
-                    $pluginManager->disablePlugin($plugin);
-                    $pluginManager->enablePlugin($plugin);
-                }
-            }
-            
-            $sender->sendMessage(TextFormat::GREEN . "All plugins have been restarted.");
-            return true;
-        }
+		if(count($args) === 0 || strtolower($args[0] ?? "") === "all"){
+			$sender->sendMessage(TextFormat::YELLOW . "Restarting all plugins...");
 
-        if(strtolower($args[0]) === "plugin"){
-            if(!isset($args[1])){
-                $sender->sendMessage(TextFormat::RED . "Usage: /reload plugin <plugin_name>");
-                return true;
-            }
+			$pluginManager = $server->getPluginManager();
+			$plugins = $pluginManager->getPlugins();
+			foreach($plugins as $plugin){
+				if($plugin->isEnabled()){
+					$pluginManager->disablePlugin($plugin);
+					$pluginManager->enablePlugin($plugin);
+				}
+			}
 
-            $pluginName = $args[1];
-            $plugin = $server->getPluginManager()->getPlugin($pluginName);
+			$sender->sendMessage(TextFormat::GREEN . "All plugins have been restarted.");
+			return true;
+		}
 
-            if($plugin === null){
-                $sender->sendMessage(TextFormat::RED . "Plugin '" . $pluginName . "' not found.");
-                return true;
-            }
+		if(strtolower($args[0] ?? "") === "plugin"){
+			if(!isset($args[1])){
+				$sender->sendMessage(TextFormat::RED . "Usage: /reload plugin <plugin_name>");
+				return true;
+			}
 
-            $sender->sendMessage(TextFormat::YELLOW . "Restarting plugin '" . $pluginName . "'...");
+			$pluginName = $args[1];
+			$plugin = $server->getPluginManager()->getPlugin($pluginName);
 
-            $pluginManager = $server->getPluginManager();
-            $pluginManager->disablePlugin($plugin);
-            $pluginManager->enablePlugin($plugin);
+			if($plugin === null){
+				$sender->sendMessage(TextFormat::RED . "Plugin '" . $pluginName . "' not found.");
+				return true;
+			}
 
-            $sender->sendMessage(TextFormat::GREEN . "Plugin '" . $pluginName . "' has been restarted.");
-            return true;
-        }
-        
-        $sender->sendMessage(TextFormat::RED . "Unknown subcommand. Use:");
-        $sender->sendMessage(TextFormat::YELLOW . "/reload [all]");
-        $sender->sendMessage(TextFormat::YELLOW . "/reload plugin <plugin_name>");
-        return true;
-    }
+			$sender->sendMessage(TextFormat::YELLOW . "Restarting plugin '" . $pluginName . "'...");
+
+			$pluginManager = $server->getPluginManager();
+			$pluginManager->disablePlugin($plugin);
+			$pluginManager->enablePlugin($plugin);
+
+			$sender->sendMessage(TextFormat::GREEN . "Plugin '" . $pluginName . "' has been restarted.");
+			return true;
+		}
+		
+		$sender->sendMessage(TextFormat::RED . "Unknown subcommand. Use:");
+		$sender->sendMessage(TextFormat::YELLOW . "/reload [all]");
+		$sender->sendMessage(TextFormat::YELLOW . "/reload plugin <plugin_name>");
+		return true;
+	}
 }
